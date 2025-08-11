4346383d0648298aa6a6dbd00840bf6ec0ae10f1
bokaco15 <bborislavilic@gmail.com>
2025-08-11 13:32:03 +0200

    * Bonus homework

--- domaci11/database/seeders/UserCreateSeeder.php
+++ domaci11/database/seeders/UserCreateSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserCreateSeeder extends Seeder
 {
@@ -12,6 +14,39 @@ class UserCreateSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $email = $this->command->getOutput()->ask('Enter email');
+        $name = $this->command->getOutput()->ask('Enter Name');
+        $password = Hash::make($this->command->getOutput()->ask('Enter password'));
+
+//        dump($email, $password, $name);
+
+        if(!$email or !$name or !$password)
+        {
+            $this->command->getOutput()->error("Greska, sva polja su obavezna");
+        }
+        else
+        {
+            if(User::where('email', $email)->exists())
+            {
+                $this->command->getOutput()->error("Greska!! Email {$email} vec postoji");
+            }
+            else
+            {
+                User::create([
+                    'email'=>$email,
+                    'name'=>$name,
+                    'password'=>$password
+                ]);
+
+                $this->command->info("Uspesno ste unijeli korisnika sa podacima:\n
+                Email: {$email} \n
+                Name: {$name} \n
+                Password: {$password}
+                ");
+
+
+            }
+        }
+
     }
 }
