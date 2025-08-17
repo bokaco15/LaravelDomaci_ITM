697e80e2df31e5afcee80bc2afe539c80fea027b
Borislav Ilic <bborislavilic@gmail.com>
2025-08-17 21:51:11 +0200

    * Seeder 'CitiesSeeder' finished * Added 100 Cities in table

--- domaci12/database/seeders/CitiesSeeder.php
+++ domaci12/database/seeders/CitiesSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cities;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,40 @@ class CitiesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        // 2. korak -> napraviti faker i pozvati klasu Factori i metodu Create za pravljenje fakera
+        $faker = Factory::create();
+
+        // 1. korak -> Napraviti komandu za broj gradova
+        $numberOfCities = $this->command->getOutput()->ask('Koliko zelite da kreirate gradova?');
+        if(!$numberOfCities) {
+            $this->command->getOutput()->error('Greska!!! Uneiste broj gradova!');
+            return;
+        }
+
+        $this->command->getOutput()->progressStart($numberOfCities);
+
+        $counter = 0;
+
+        //3. Korak napraviti da se upisuje grad u tabelu Cities
+        for($i=0; $i<$numberOfCities; $i++) {
+            $city = $faker->unique()->city();
+            if(Cities::where('name', $city)->exists()) {
+                $this->command->getOutput()->error("Greska!!, $city vec postoji");
+                continue;
+            }
+                Cities::create([
+                    'name'=>$city
+                ]);
+
+                $counter++;
+
+                $this->command->getOutput()->progressAdvance();
+        }
+
+        $this->command->getOutput()->progressFinish();
+
+        $this->command->getOutput()->success("Uspesno ste uneli {$counter} grada tabelu");
+
     }
 }
