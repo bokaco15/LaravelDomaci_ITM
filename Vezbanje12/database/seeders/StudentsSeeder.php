1efc25c6c6a0e41b887fe40ac7e11a39a2732054
bokaco15 <bborislavilic@gmail.com>
2025-08-18 14:25:41 +0200

    * Students seeder added students to table

--- Vezbanje12/database/seeders/StudentsSeeder.php
+++ Vezbanje12/database/seeders/StudentsSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,27 @@ class StudentsSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->command->getOutput()->info('test test');
+        $noStudents = (int)$this->command->getOutput()->ask('Koliko studenata zelite da unesete');
+        $faker = Factory::create();
+
+        $this->command->getOutput()->progressStart($noStudents);
+        $counter = 0;
+
+        for($i=0; $i<$noStudents; $i++)
+        {
+            Student::create([
+               'name' => $faker->name(),
+                'email' => $faker->unique()->email(),
+                'enrollment_date'=>$faker->date()
+            ]);
+
+            $counter++;
+
+            $this->command->getOutput()->progressAdvance();
+        }
+
+        $this->command->getOutput()->progressFinish();
+        $this->command->getOutput()->success("Uspjeso ste dodali $counter studenata");
+
     }
 }
