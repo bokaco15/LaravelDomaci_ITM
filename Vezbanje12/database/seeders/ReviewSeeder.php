8d82dcd847674f4a82f2e07582e20143c523558d
bokaco15 <bborislavilic@gmail.com>
2025-08-19 12:00:06 +0200

    * Review Seeder [finish]

--- Vezbanje12/database/seeders/ReviewSeeder.php
+++ Vezbanje12/database/seeders/ReviewSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,23 @@ class ReviewSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Factory::create();
+        $books = Book::all();
+
+        foreach ($books as $book)
+        {
+            $numOfReviews = mt_rand(2,4);
+            for($i=0; $i<$numOfReviews; $i++)
+            {
+                Review::create([
+                    'book_id' => $book->id,
+                    'rate' => mt_rand(1,5),
+                    'comment' => $faker->text(100)
+                ]);
+            }
+        }
+
+        $this->command->getOutput()->info('Uspijesno ste unijeli recenzije za knjige');
+
     }
 }
