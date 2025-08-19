3e05a8d2d1847d3fec0168c20b3bd2238e672257
bokaco15 <bborislavilic@gmail.com>
2025-08-19 07:29:05 +0200

    * Entollment Model/Migration [DONE]

---
+++ Vezbanje12/database/migrations/2025_08_19_051806_create_entrollments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //student_id, course_id, grade
+        Schema::create('entrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->references('id')->on('students')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('course_id')->references('id')->on('courses')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->integer('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entrollments');
+    }
+};
